d5b835de855b9bfcfede75f32729014b4dee4a8d
abdul dakkak <abduld@wolfram.com>
2017-10-02 10:37:59 -0500

    Update lib.go

--- mxnet/lib.go
+++ mxnet/lib.go
@@ -1,7 +1,7 @@
 package mxnet
 
 // #cgo amd64 pkg-config: mxnet
-// #cgo linux ppc64le CFLAGS: -I /home/carml/frameworks/mxnet/include
-// #cgo linux ppc64le LDFLAGS: -L /home/carml/frameworks/mxnet/lib -lmxnet 
+// #cgo linux,ppc64le CFLAGS: -I /home/carml/frameworks/mxnet/include -I /opt/mxnet/include
+// #cgo linux,ppc64le LDFLAGS: -L /home/carml/frameworks/mxnet/lib -L /opt/mxnet/lib -lmxnet 
 import "C"
 
