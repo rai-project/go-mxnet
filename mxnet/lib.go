b096c1c709f9e93114c4927ef3f8432c1cc135db
Cheng Li <cli99@illinois.edu>
2017-10-31 17:48:05 -0500

    udpate lib

--- mxnet/lib.go
+++ mxnet/lib.go
@@ -1,7 +1,6 @@
 package mxnet
 
-// #cgo amd64 pkg-config: mxnet
-// #cgo linux,ppc64le CFLAGS: -I /home/carml/frameworks/mxnet/include -I /opt/mxnet/include
-// #cgo linux,ppc64le LDFLAGS: -L /home/carml/frameworks/mxnet/lib -L /opt/mxnet/lib -lmxnet 
+// #cgo darwin,amd64 pkg-config: mxnet
+// #cgo linux CFLAGS: -I /home/carml/frameworks/mxnet/include -I /opt/mxnet/include
+// #cgo linux LDFLAGS: -L /home/carml/frameworks/mxnet/lib -L /opt/mxnet/lib -lmxnet
 import "C"
-
