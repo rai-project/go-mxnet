cfa888c1d56b94790247bff7c9dd193943c17a75
Cheng Li <cli99@illinois.edu>
2017-12-08 08:41:17 +0800

    update lib on oxs

--- mxnet/lib.go
+++ mxnet/lib.go
@@ -1,6 +1,7 @@
 package mxnet
 
-// #cgo darwin,amd64 pkg-config: mxnet
+// #cgo CFLAGS: -I/opt/mxnet/include
+// #cgo LDFLAGS: -L/opt/mxnet/lib -lmxnet
 // #cgo linux CFLAGS: -I /home/carml/frameworks/mxnet/include -I /opt/mxnet/include  -I /opt/frameworks/mxnet/include
 // #cgo linux LDFLAGS: -L /home/carml/frameworks/mxnet/lib -L /opt/mxnet/lib -L /opt/frameworks/mxnet/lib -lmxnet
 import "C"
