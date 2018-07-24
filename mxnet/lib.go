311c8ef2fcfabf49e90c56d6fe0fe77e77a57307
abhiutd <abhibits2012@gmail.com>
2018-07-24 13:23:38 -0500

    binding working

--- mxnet/lib.go
+++ mxnet/lib.go
@@ -2,6 +2,6 @@ package mxnet
 
 // #cgo CFLAGS: -I/home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/include
 // #cgo LDFLAGS: -L/home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/lib -lmxnet
-// #cgo linux CFLAGS: -I /home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/include
-// #cgo linux LDFLAGS: -L /home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/lib -lmxnet
+// #cgo linux CFLAGS: -I/home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/include
+// #cgo linux LDFLAGS: -L/home/as29/my_mxnet/apache-mxnet-src-1.2.0-incubating/lib -lmxnet
 import "C"
