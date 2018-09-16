eb365efe948b7e6da3f10b7eed69942ef7e43ca8
Abdul Dakkak <dakkak@illinois.edu>
2018-09-16 02:31:32 +0000

    updates

--- mxnet/lib.go
+++ mxnet/lib.go
@@ -4,6 +4,6 @@ package mxnet
 // #cgo LDFLAGS: -lmxnet
 // #cgo darwin CFLAGS: -I/Users/abduld/frameworks/mxnet/include
 // #cgo darwin LDFLAGS: -L/Users/abduld/frameworks/mxnet/lib
-// #cgo linux CFLAGS: -I/opt/frameworks/mxnet/include -I/home/abduld/frameworks/mxnet/include
-// #cgo linux LDFLAGS: -L/opt/frameworks/mxnet/lib -L/home/abduld/frameworks/mxnet/lib
+// #cgo linux CFLAGS: -I/opt/mxnet/include
+// #cgo linux LDFLAGS: -L/opt/mxnet/lib
 import "C"
